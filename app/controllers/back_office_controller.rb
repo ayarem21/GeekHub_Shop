4131530de56e1442b10536a5ea531b91f533bb51
ayarem21 <ayarem21@gmail.com>
2019-12-24 22:31:08 +0200

    add paginate

--- app/controllers/back_office_controller.rb
+++ app/controllers/back_office_controller.rb
@@ -1,3 +1,8 @@
 class BackOfficeController < ApplicationController
+  before_action :check_admin
   layout "backoffice"
+
+  def check_admin
+    redirect_to "/product" unless current_user&.admin?
+  end
 end
