894a03f94618f754756c2a06ac5c46bafc537af4
ayarem21 <ayarem21@gmail.com>
2020-01-19 14:54:10 +0200

    rating with radio button

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,23 +1,23 @@
 class ProductController < ApplicationController
   before_action :set_product, only: [:show]
   def index
-    @products = Product.all.order('created_at DESC').paginate(page: params[:page], per_page: 8)
+    @products = Product.all.order('created_at DESC').paginate(page: params[:page], per_page: 15)
   end
 
   def show; end
 
   def min_price
-    @products = Product.order_by_price_min.paginate(page: params[:page], per_page: 8)
+    @products = Product.order_by_price_min.paginate(page: params[:page], per_page: 15)
     render action: :index
   end
 
   def max_price
-    @products = Product.order_by_price_max.paginate(page: params[:page], per_page: 8)
+    @products = Product.order_by_price_max.paginate(page: params[:page], per_page: 15)
     render action: :index
   end
 
   def new_sort
-    @products = Product.order_by_new.paginate(page: params[:page], per_page: 8)
+    @products = Product.order_by_new.paginate(page: params[:page], per_page: 15)
     render action: :index
   end
 
